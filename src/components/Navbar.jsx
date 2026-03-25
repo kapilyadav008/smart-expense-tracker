a25e39cbdfbe1bcb80f159b029cc098ec5bb8693
kapilyadav008 <kapilyadavtps972062@gmail.com>
2026-03-25 19:40:59 +0530

    Build navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className='flex justify-between items-center p-4 bg-gray-800 text-white'>
+        <h2>Expense Tracker</h2>
+    </nav>
+  )
+}
+
+export default Navbar
